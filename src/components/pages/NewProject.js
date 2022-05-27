a6c657893c3ceb1cf4e34d937aa39271ed353206
eliabe dos santos <eliabe.s.o36@gmail.com>
2022-05-27 14:03:16 -0300

    init new project page

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,38 @@
+//General
+import { Link } from 'react-router-dom';
+
+// Layout
+import { Container, Row, Col } from 'react-bootstrap';
+import LinkButton from '../layout/LinkButton';
+
+// Styles 
+import styles from './modules/NewProject.module.css';
+
+// Images
+import project from '../../img/project.png';
+
 function NewProject(){
     return (
-        <div>
-        <h1>New Project</h1>
-        </div>
+        <section className="NewProject py-5">
+            <Container>
+                <Row>
+                    <Col className={`d-flex justify-content-center`}>
+                        <h1>Criar Projeto</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6} className={``}>
+                            <p className={`${styles.text__description}`}>
+                               
+                            </p>
+                        <img src={project} alt="Project manneger"  className="w-75 mw-100" />
+                    </Col>
+                    <Col md={6} className={`d-flex justify-content-center`}>
+                      
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 }
 export default NewProject;
